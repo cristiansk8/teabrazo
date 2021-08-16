5fc6bcc0755cfa8a88b9b079e448db79f05eeaab
vanegasroberto <Vanegasroberto@gmail.com>
2021-08-16 18:53:39 -0500

    se modifica sección block bannerr@nosotros

--- wp-content/themes/teabrazo/includes/nosotros/block-banner.php
+++ wp-content/themes/teabrazo/includes/nosotros/block-banner.php
@@ -18,19 +18,13 @@
         $ms_title = get_sub_field('block_banner__title');
         $ms_text = get_sub_field('block_banner__text');        
      ?>
-      <div class="block-banner--<?php echo $ms_bg; ?> row g-0">
+      <div class="block-banner">
         <div class="col-lg-6">
-          <?php if ($ms_link) : ?>
-            <a href="<?php echo $ms_link; ?>">
-          <?php endif; ?>
           <picture class="block-banner__image img-responsive">          
             <?php if ($ms_image) : ?>
               <img src="<?php the_sub_field('block_banner__image')?>" />
             <?php endif; ?>
           </picture>
-          <?php if ($ms_link) : ?>
-            </a>
-          <?php endif; ?>
         </div>
         <div class="col-lg-6">
           <div class="block-banner__content">
@@ -39,17 +33,7 @@
                 <?php echo $ms_title; ?>
               </h2>
               <p class="block-banner__text"><?php echo $ms_text; ?></p>
-            <?php endif; ?>
-            <?php if ($ms_image_side) : ?>
-              <figure class="block-banner__side-img">
-                <img src="<?php echo esc_url($ms_image_side['url']); ?>"/>
-              </figure>
-            <?php endif; ?>
-            <?php if ($ms_link) : ?>
-              <a href="<?php echo $ms_link; ?>" class="c-button">
-                <span class="c-button__content"><?php echo $ms_text_link; ?></span>
-              </a>
-            <?php endif; ?>
+            <?php endif; ?> 
           </div>
         </div>
       </div>
