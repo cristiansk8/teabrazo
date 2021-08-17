8c875be3b132204bf1eafb0131b5914d744ed0f1
vanegasroberto <Vanegasroberto@gmail.com>
2021-08-16 21:08:47 -0500

    ajustes en servicios@nuestros servicios

--- wp-content/themes/teabrazo/includes/servicios/servicios.php
+++ wp-content/themes/teabrazo/includes/servicios/servicios.php
@@ -1,31 +1,28 @@
 <section id="servicios">
-  <div class="container-servicios">
+  <div class="container-servicios"> 
     <div class="row container-servicios">
         <?php
         while(have_rows('servicios')): the_row();
         ?>
       
         <div class="servicios col-sm-4">
-            <picture>
-              <img class= "servicios__imagen img-responsive" src="<?php the_sub_field('imagen') ?>" alt="">
-              
-            </picture>
+            <div>
+              <picture>
+                <img class= "servicios__imagen img-responsive" src="<?php the_sub_field('imagen') ?>" alt="">              
+              </picture>
+            </div>
             <div class="servicios__titulo">
-                    <h4>
-                       <?php the_sub_field('titulo') ?>
-                    </h4>
-                </div>
+                <h4>
+                  <?php the_sub_field('titulo') ?>
+                </h4>
+            </div>
             <div class="servicios__caption">
-              <?php the_sub_field('info') ?>
+                <?php the_sub_field('info') ?>
             </div>
             <div class="servicios__enlace">
-              <a  href="<?php the_sub_field('link') ?> "target="_blank" > Contáctanos</a>
+                <a  href="<?php the_sub_field('link') ?> "target="_blank" >Contáctanos</a>
             </div>
         </div>
-      
-      
-
-
       <?php endwhile; ?>
     </div>
   </div>
